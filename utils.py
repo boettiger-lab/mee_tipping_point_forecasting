5f5b268260d64d34ae2494d7cf45044e02f71b16
mlap <marcuslapeyrolerie@me.com>
2022-03-15 15:45:29 +0000

    Adding helper file

---
+++ utils.py
@@ -0,0 +1,11 @@
+def sliding_windows(data, seq_length):
+    x = []
+    y = []
+
+    for i in range(len(data)-seq_length-1):
+        _x = data[i:(i+seq_length)]
+        _y = data[i+seq_length]
+        x.append(_x)
+        y.append(_y)
+
+    return np.array(x),np.array(y)
