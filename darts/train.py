5c07d41237349ebac52dc508973d3c74f0d08f06
mlap <marcuslapeyrolerie@me.com>
2022-03-31 23:58:25 +0000

    Giving more informative plots now

--- darts/train.py
+++ darts/train.py
@@ -10,7 +10,7 @@ from model import tipping_point
 from darts import TimeSeries
 from darts.models import RNNModel, TCNModel, TransformerModel
 from darts.utils.likelihood_models import LaplaceLikelihood
-from utils import preprocessed_t_series
+from utils import preprocessed_t_series, truth_dist
 import argparse
 import yaml
 
@@ -49,6 +49,7 @@ parser.add_argument(
     help="Seed selection",
 )
 args = parser.parse_args()
+np.random.seed(42)
 
 if args.model == "lstm":
     from train_hyperparams.lstm import hyperparameters
@@ -81,10 +82,12 @@ if args.plot:
     colors = ["blue", "green", "orange"]
     for i in range(3):
         train_series = preprocessed_t_series(1)
-        train_series[:100].plot(color=colors[i], label='_nolegend_')
+        train_series[:100].plot(color="blue", label='truth')
         t_series, v_series = train_series.split_before(25)
+        t_dist = truth_dist(t_series, n_samples=100)
+        t_dist[:75].plot(low_quantile=0.025, high_quantile=0.975, linestyle="", color="blue", label="_nolegend_")
         preds = my_model.predict(75, t_series, num_samples=10000)
-        preds.plot(low_quantile=0.15, high_quantile=0.85, linestyle="dotted", color=colors[i], label='_nolegend_')
+        preds.plot(low_quantile=0.025, high_quantile=0.975, linestyle="dotted", color="orange", label='prediction')
 
-plt.savefig(f"plots/{args.output_file_name}")
-plt.clf()
+        plt.savefig(f"plots/{args.output_file_name}_{i}")
+        plt.clf()
