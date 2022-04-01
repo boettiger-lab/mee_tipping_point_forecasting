1f4fb39e0000fa80c12c39b94e2092123f7dcbfa
mlap <marcuslapeyrolerie@me.com>
2022-04-01 18:01:28 +0000

    Maybe missed something here

--- darts/utils.py
+++ darts/utils.py
@@ -18,3 +18,20 @@ def preprocessed_t_series(n_samples):
 
     vals = np.array(_ts).reshape(training_data.shape[1], 1, n_samples)
     return TimeSeries.from_times_and_values(RangeIndex(250),vals)
+
+def truth_dist(t_series, n_samples=100):
+    N_init = t_series[-1].values()[0][0]
+    t_max = 125
+    _data = tipping_point(N_init, t_max)
+    for i in range(n_samples):
+      training_data = _data.collect_samples(n_samples)
+      
+      # Preprocessing time series to
+      _ts = [[] for i in range(training_data.shape[1])]
+      for i in range(n_samples):
+          for j in range(training_data.shape[1]):
+              _ts[j].append(training_data[i, j])
+  
+      vals = np.array(_ts).reshape(training_data.shape[1], 1, n_samples)
+    return TimeSeries.from_times_and_values(RangeIndex(start=25, stop=25+t_max),vals)
+        
