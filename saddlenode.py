751449aa97e4d74359882ae252fa8a61d894f0e8
Carl <cboettig@gmail.com>
2022-04-05 03:56:20 +0000

    cb saddlenode

--- saddlenode.py
+++ saddlenode.py
@@ -1,22 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class tipping_point():
+class saddlenode():
     def __init__(self, N=0.75, t_max=250):
+        params={
+                "r": 0.7,
+                "K": 1.5,
+                "sigma": 0.01,
+                "q": 3,
+                "b": 0.15,
+                "a": 0.19,
+                "M": 1.2,
+                "x0": 0.8,
+                "alpha": 0.001,
+                "beta": 1.0,
+            },
         self.replicates = 0
         self.t_max = t_max
         self.mu = -5e-4
-        self.sigma = 1e-2
+        self.sigma = params["sigma"]
         self.r = -0.0625
-        self.N_init = N
+        self.N_init = params["x0"]
         self.N = N
         self.offset = 0.5
         self.reps = 0
 
+    def may(x, params, size=1):
+        with np.errstate(divide="ignore"):
+            r = params["r"]
+            M = params["M"]
+            a = params["a"]
+            q = params["q"]
+            b = params["b"]
+            exp_mu = (
+                x
+                + x * r * (1 - x / M)
+                - a * np.power(x, q) / (np.power(x, q) + np.power(b, q))
+            )
+            mu = np.log(np.clip(exp_mu, 0, np.inf))
+        state = np.random.lognormal(mu, params["sigma"], size)
+        return np.maximum(0, state)
+
     def step(self):
         self.eta = np.random.normal(self.mu, self.sigma)
-        self.N += (0.75 - self.N)**2 * (0.25 - self.N) + self.eta
-        self.N = np.clip(self.N, 0, 1)
+        self.N = self.may(self.N, params)
         return self.N
 
     def draw_replicate(self):
