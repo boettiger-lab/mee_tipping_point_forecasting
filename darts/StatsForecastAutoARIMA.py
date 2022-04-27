9fadf5249caf56f5f81455e548522d7e5f3cda50
mlap <marcuslapeyrolerie@me.com>
2022-04-27 00:48:52 +0000

    Cleaning up a bit

--- darts/StatsForecastAutoARIMA.py
+++ darts/StatsForecastAutoARIMA.py
@@ -1,4 +1,4 @@
-from darts.models import StatsForecastAutoARIMA
+from darts.models import StatsForecastAutoARIMA, KalmanForecaster, NaiveDrift
 from darts import TimeSeries
 import sys
 sys.path.append("../")
@@ -7,6 +7,7 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from utils import preprocessed_t_series, truth_dist
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -22,6 +23,13 @@ parser.add_argument(
     type=str,
     help="Select which case for the training series",
 )
+parser.add_argument(
+    "-m",
+    "--model",
+    default="arima",
+    type=str,
+    help="Select which model to train",
+)
 args = parser.parse_args()
 
 if args.case.lower() == "tipped" and args.tp_model == "stochastic":
@@ -32,10 +40,16 @@ elif args.case.lower() == "both" and args.tp_model == "stochastic":
     train_series = TimeSeries.from_csv("stochastic_tipped.csv", time_col="time")
     train_series_ = TimeSeries.from_csv("stochastic_non_tipped.csv", time_col="time")
     train_series = train_series.concatenate(train_series_, axis="sample")
-        
+
+_model = {"arima" : StatsForecastAutoARIMA, "naive" : NaiveDrift}[args.model]
 input_len =25
 output_len=24
-for i in range(3):
+
+if not os.path.exists("plots/"):
+        os.makedirs("plots/")
+if not os.path.exists(f"plots/{args.model}"):
+        os.makedirs(f"plots/{args.model}")
+for i in range(1):
     train_series[:input_len].plot(color="blue", label='truth')
     t_series, v_series = train_series.split_before(input_len)
     t_dist = truth_dist("stochastic", t_series, input_len, output_len, n_samples=100)
@@ -43,11 +57,9 @@ for i in range(3):
     t_max = 250-input_len
     
     t_dist.plot(low_quantile=0.025, high_quantile=0.975, color="blue", label="_nolegend_", linestyle="dotted")
-    model = StatsForecastAutoARIMA().fit(t_series)
+    model = _model().fit(t_series)
     preds = model.predict(t_max, num_samples=100)
     preds.plot(low_quantile=0.025, high_quantile=0.975, linestyle="dotted", color="orange", label='prediction')
     
-    if not os.path.exists("plots/"):
-        os.makedirs("plots/")
-    plt.savefig(f"plots/StatsForecastAutoARIMA_{i}")
+    plt.savefig(f"plots/{args.model}/{i}")
     plt.clf()
