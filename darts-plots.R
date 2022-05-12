10e111041faef9fae16cd0d22e9fadbdc6d56521
Carl <cboettig@gmail.com>
2022-05-12 00:47:51 +0000

    tweaking plots, some writing
    added a bit explicitly about frameworks

--- darts-plots.R
+++ darts-plots.R
@@ -9,7 +9,6 @@ reference <- arrow::open_dataset("greta/forecasts/", format="csv") |>
   filter(type != "predicted") |> 
   collect()
 
-
 sims <- reference |>
   group_by(simulation, variable, type, t) |> 
   summarise(mean = mean(value), 
@@ -22,10 +21,12 @@ sims <- reference |>
                                         X = "N")) 
 
 
-darts <- arrow::open_dataset("darts/forecasts/", format="csv")
-darts <- darts |> filter(type == "predicted")
-greta <- arrow::open_dataset("greta/forecasts/", format="csv") |> mutate(forecasting_model = "MCMC", reps=1, group=1)
-greta <- greta |>  filter(type == "predicted") |> rename(iter = i)
+darts <- arrow::open_dataset("darts/forecasts/", format="csv") |>
+  filter(type == "predicted")
+greta <- arrow::open_dataset("greta/forecasts/", format="csv") |>
+  mutate(forecasting_model = "MCMC", reps=1, group=1) |>
+  filter(type == "predicted") |> 
+  rename(iter = i)
 
   
   
@@ -50,41 +51,44 @@ combined <- bind_rows(darts_df, greta_df) |>
                                         host = "H", 
                                         parasitoid = "P", 
                                         X = "N")) 
+arrow::write_parquet(combined, "manuscript/data/forecasts.parquet")
+arrow::write_parquet(sims, "manuscript/data/observations.parquet")
 
-hopf_sims <- sims |> filter(simulation=="hopf")
-stochastic_sims <- sims |> filter(simulation=="stochastic")
-saddle_sims <- sims |> filter(simulation=="saddle")
 
-combined |> filter(simulation=="hopf", reps == 1, group == 1) |> 
+forecasts <- read_parquet("manuscript/data/forecasts.parquet")
+observations <- read_parquet("manuscript/data/observations.parquet")
+
+hopf_obs <- observations |> filter(simulation=="hopf")
+stochastic_obs <- observations |> filter(simulation=="stochastic")
+saddle_obs <- observations |> filter(simulation=="saddle")
+
+forecasts |> filter(simulation=="hopf", reps == 1, group == 1) |> 
   ggplot(aes(t, col=type, fill=type)) + 
   geom_ribbon(aes(ymin = lower, ymax = upper), alpha=0.5) +
   geom_ribbon(aes(ymin = lower, ymax = upper), alpha=0.5, data = hopf_sims) +
   geom_line(aes(y=mean)) +
-  geom_line(aes(y=mean), data = hopf_sims) +
+  geom_line(aes(y=mean), data = hopf_obs) +
   facet_grid(forecasting_model ~ variable, scales = "free") +
   theme_bw() + 
   ggtitle("A. Hopf bifurcation")
 
-
-
-
-combined |> filter(simulation=="saddle", group == 1, reps == 1) |> 
+forecasts |> filter(simulation=="saddle", group == 1, reps == 1) |> 
   ggplot(aes(t, col=type, fill=type)) + 
   geom_ribbon(aes(ymin = pmax(lower,0), ymax = upper), alpha=0.5) +
   geom_ribbon(aes(ymin = pmax(lower,0), ymax = upper), alpha=0.5, data = saddle_sims) +
   geom_line(aes(y=mean)) +
-  geom_line(aes(y=mean), data = saddle_sims) +
+  geom_line(aes(y=mean), data = saddle_obs) +
   facet_wrap(~forecasting_model, scales = "free") +
   #facet_grid(forecasting_model ~ reps, scales = "free") +
   theme_bw() + 
   ggtitle("B. Saddle-Node bifurcation")
 
-combined |> filter(simulation=="stochastic", group == 1, reps==1) |> 
+forecasts |> filter(simulation=="stochastic", group == 1, reps==1) |> 
   ggplot(aes(t, col=type, fill=type)) + 
   geom_ribbon(aes(ymin = pmax(lower,0), ymax = upper), alpha=0.5) +
   geom_ribbon(aes(ymin = pmax(lower,0), ymax = upper), alpha=0.5, data = stochastic_sims) +
   geom_line(aes(y=mean)) +
-  geom_line(aes(y=mean), data = stochastic_sims) +
+  geom_line(aes(y=mean), data = stochastic_obs) +
   facet_wrap(~forecasting_model, scales = "free") +
   #facet_grid(forecasting_model ~ reps, scales = "free") +
   theme_bw() + 
